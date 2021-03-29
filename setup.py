e670ed7ab40fbb7a67be5905393c529b4144f811
Kabilar Gunalan <kabilar@vathes.com>
2021-03-29 16:42:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,22 +6,22 @@ import sys
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
-DataJoint Element for multi-photon calcium imaging data analyzed with Suite2p and/or CaImAn.
+DataJoint Element for miniscope calcium imaging data.
 """
 
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='element-calcium-imaging',
+    name='element-miniscope',
     version='0.0.1',
-    description="Calcium Imaging DataJoint element",
+    description="Miniscope DataJoint Element",
     long_description=long_description,
     author='DataJoint NEURO',
     author_email='info@vathes.com',
     license='MIT',
-    url='https://github.com/datajoint/element-calcium-imaging',
-    keywords='neuroscience calcium-imaging science datajoint',
+    url='https://github.com/datajoint/element-miniscope',
+    keywords='neuroscience calcium-imaging science datajoint miniscope',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     scripts=[],
     install_requires=requirements,
