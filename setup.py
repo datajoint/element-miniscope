ca8b5386961f53e49107bcc0d1d1e9b71f74f9a1
Kabilar Gunalan <kabilar@vathes.com>
2021-04-01 00:43:39 -0500

    Update package metadata handling

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
+pkg_name = 'element_miniscope'
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
@@ -12,9 +13,12 @@ DataJoint Element for miniscope calcium imaging data.
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
+with open(path.join(here, pkg_name, 'version.py')) as f:
+    exec(f.read())
+
 setup(
     name='element-miniscope',
-    version='0.0.1',
+    version=__version__,
     description="Miniscope DataJoint Element",
     long_description=long_description,
     author='DataJoint NEURO',
@@ -23,6 +27,5 @@ setup(
     url='https://github.com/datajoint/element-miniscope',
     keywords='neuroscience calcium-imaging science datajoint miniscope',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    scripts=[],
     install_requires=requirements,
 )
