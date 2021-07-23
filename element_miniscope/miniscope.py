3a2116861df6c35c70acffb5599077dcb61ec9e4
shenshan <shenshanpku@gmail.com>
2021-07-23 10:42:07 -0700

    remove scan module

--- element_miniscope/miniscope.py
+++ element_miniscope/miniscope.py
@@ -386,6 +386,22 @@ class MotionCorrection(dj.Imported):
         block_count_z                   : int           # number of blocks tiled in the z direction
         """
 
+    class Block(dj.Part):
+        definition = """  # FOV-tiled blocks used for non-rigid motion correction
+        -> master.NonRigidMotionCorrection
+        block_id        : int
+        ---
+        block_y         : longblob  # (y_start, y_end) in pixel of this block
+        block_x         : longblob  # (x_start, x_end) in pixel of this block
+        block_z         : longblob  # (z_start, z_end) in pixel of this block
+        y_shifts        : longblob  # (pixels) y motion correction shifts for every frame
+        x_shifts        : longblob  # (pixels) x motion correction shifts for every frame
+        z_shifts=null   : longblob  # (pixels) x motion correction shifts for every frame
+        y_std           : float     # (pixels) standard deviation of y shifts across all frames
+        x_std           : float     # (pixels) standard deviation of x shifts across all frames
+        z_std=null      : float     # (pixels) standard deviation of z shifts across all frames
+        """
+
     class Summary(dj.Part):
         definition = """ # summary images for each field and channel after corrections
         -> master
